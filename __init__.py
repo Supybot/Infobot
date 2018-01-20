a56d9b6a18f3d26c5408cd8a15a77f6698be644a
James McCoy <jamessan@jamessan.com>
2018-01-19 19:57:44 -0500

    Use relative imports within package

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 ###
-# Copyright (c) 2004-2005, James Vega
+# Copyright (c) 2004-2018, James McCoy
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -47,14 +47,14 @@ __contributors__ = {}
 # This is a url where the most recent plugin package can be downloaded.
 __url__ = '' # 'http://supybot.com/Members/yourname/Infobot/download'
 
-import config
-import plugin
+from . import config
+from . import plugin
 reload(plugin) # In case we're being reloaded.
 # Add more reloads here if you add third-party modules and want them to be
 # reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
-    import test
+    from . import test
 
 Class = plugin.Class
 configure = config.configure
